391496834cccd9f01a527e4289e2440207090ed0
Sakthivel <drsakthivelsankaran2024@gmail.com>
2026-02-27 09:01:10 +0530

    Improve build.sh: ensure migrations always run during build

--- build.sh
+++ build.sh
@@ -3,10 +3,18 @@ set -e
 
 echo "Building portfolio application..."
 
+# Upgrade pip
+pip install --upgrade pip
+
+# Install Python dependencies (should already be installed, but ensure)
+pip install -r requirements.txt
+
 # Collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 # Run migrations
+echo "Running migrations..."
 python manage.py migrate
 
 echo "Build complete!"
