9f4e58b68238421da9ea9ebd06e8a5bb7791098c
Sakthivel <drsakthivelsankaran2024@gmail.com>
2026-03-01 00:04:51 +0530

    Use StaticFilesStorage without manifest requirement

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -169,7 +169,7 @@ if config('SUPABASE_URL', default=''):
             }
         },
         'staticfiles': {
-            'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
+            'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
         }
     }
     MEDIA_URL = config('SUPABASE_URL') + '/storage/v1/object/public/portfolio/'
@@ -180,7 +180,7 @@ else:
             'BACKEND': 'django.core.files.storage.FileSystemStorage',
         },
         'staticfiles': {
-            'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
+            'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
         }
     }
 
